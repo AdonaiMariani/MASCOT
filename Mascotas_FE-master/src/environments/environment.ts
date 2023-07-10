22ffa1c0a8f4c545903d10cb3e7f9f9f374711e7
AdonaiMariani <110501772+AdonaiMariani@users.noreply.github.com>
2023-07-10 16:20:17 -0300

    Update environment.ts

--- Mascotas_FE-master/src/environments/environment.ts
+++ Mascotas_FE-master/src/environments/environment.ts
@@ -2,11 +2,14 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//export const environment = {
+//  production: false,
+//  endpoint: 'https://localhost:7045/'
+//};
 export const environment = {
   production: false,
-  endpoint: 'https://localhost:7045/'
+  apiUrl: 'https://localhost:7045/api'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
